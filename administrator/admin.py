24866e900dbc3be905133179befbfd8714e707f5
Hugo <charlesugo000@gmail.com>
2019-01-05 17:16:14 +0100

    'Done'

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from administrator.models import BitCoinBuyPrice, BitCoinSellPrice, RippleSellPrice, EthereumSellPrice, PerfectMoneySellPrice
+from administrator.models import BitCoinBuyPrice, RippleBuyPrice, EthereumBuyPrice, PerfectMoneyBuyPrice
+from administrator.models import BitCoinSellPrice, RippleSellPrice, EthereumSellPrice, PerfectMoneySellPrice
 from registered_user.models import CompleteUserRegistration
 
 admin.site.register(BitCoinBuyPrice)
+admin.site.register(RippleBuyPrice)
+admin.site.register(EthereumBuyPrice)
+admin.site.register(PerfectMoneyBuyPrice)
+
 admin.site.register(CompleteUserRegistration)
 admin.site.register(BitCoinSellPrice)
 admin.site.register(RippleSellPrice)
