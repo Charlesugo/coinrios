cf84acd32bc169cdda24e11bebb1c4e4e4ff7e58
Charles Ugo <charlesugo000@gmail.com>
2019-05-21 15:47:03 +0100

    static changes

--- frontlineExchange/settings.py
+++ frontlineExchange/settings.py
@@ -79,17 +79,24 @@ WSGI_APPLICATION = 'frontlineExchange.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coinrios_db',
-        'USER': 'frontline_u',
-        'PASSWORD': 'Frontline247',
-        'HOST': 'localhost',
-        'PORT': '',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'coinrios_db',
+            'USER': 'frontline_u',
+            'PASSWORD': 'Frontline247',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
