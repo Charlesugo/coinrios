711a2c873f7dc5fea0cd6e42e53767da8d96ebf3
Charles Ugo <charlesugo000@gmail.com>
2019-05-22 18:07:02 +0100

    update host

--- frontlineExchange/settings.py
+++ frontlineExchange/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-+-3^&j_&!4xo8&cznj(8z&uit#qxr$hwc89%l@kc(yr0_%()('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['134.209.65.206']
+ALLOWED_HOSTS = ['206.81.2.122']
 
 
 # Application definition
